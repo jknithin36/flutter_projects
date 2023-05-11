26eb450df0a7d2778fb363c51f5d38d1ee43c692
JK NITHIN <jknithin36@gmail.com>
2023-05-11 10:22:25 +0530

    quiz_app

---
+++ quiz_app/lib/questions.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+  @override
+  State<QuestionsScreen> createState() {
+    return _QuestionScreenState();
+  }
+}
+
+class _QuestionScreenState extends State<QuestionsScreen> {
+  @override
+  Widget build(context) {
+    return const Text("QuestionScreen..");
+  }
+}
